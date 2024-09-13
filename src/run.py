dfe7763d07578f7159664f37ebf1f0839d6c3be9
AlexDo1 <alexander.dolich@kit.edu>
2024-09-13 23:25:55 +0200

    first run.py to test hyras and eobs

--- src/run.py
+++ src/run.py
@@ -1,30 +1,32 @@
 import os
 from datetime import datetime as dt
-from pprint import pprint
-
+from workflows import workflow_eobs, workflow_hyras
 from json2args import get_parameter
-from json2args.data import get_data
+from json2args.data import get_data_paths
 
-# parse parameters
-kwargs = get_parameter()
-data = get_data(as_dict=True)
 
-# check if a toolname was set in env
-toolname = os.environ.get('TOOL_RUN', 'foobar').lower()
+def main():
+    # Parse parameters
+    kwargs = get_parameter()
+    data = get_data_paths(as_dict=True)
 
-# switch the tool
-if toolname == 'foobar':
-    # RUN the tool here and create the output in /out
-    print('This toolbox does not include any tool. Did you run the template?\n')
-    
-    # write parameters to STDOUT.log
-    pprint(kwargs)
+    # Check if a toolname was set in env
+    toolname = os.environ.get("TOOL_RUN", "").lower()
 
-    for name, ds in data.items():
-        print(f"\n### {name}")
-        print(ds)
-    
+    # Switch the tool
+    if toolname == "eobs":
+        # Run the E-OBS workflow
+        workflow_eobs(kwargs, data)
 
-# In any other case, it was not clear which tool to run
-else:
-    raise AttributeError(f"[{dt.now().isocalendar()}] Either no TOOL_RUN environment variable available, or '{toolname}' is not valid.\n")
+    elif toolname == "hyras":
+        # Run the HYRAS workflow
+        workflow_hyras(kwargs, data)
+
+    else:
+        # In any other case, it was not clear which tool to run
+        raise AttributeError(
+            f"[{dt.now().isocalendar()}] Either no TOOL_RUN environment variable available, or '{toolname}' is not valid.\n")
+
+
+if __name__ == "__main__":
+    main()
